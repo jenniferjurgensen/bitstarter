34c93427dddb0557f6ca33567c2194b13a5806fa
Ubuntu <ubuntu@ip-172-31-20-168.us-west-2.compute.internal>
2013-07-10 20:00:52 +0000

    Added ability to read from url for grader.js

--- grader.js
+++ grader.js
@@ -24,9 +24,9 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
-
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
@@ -61,14 +61,45 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+
+var buildfn = function(file, checksfile) {
+    var response2console = function(result, response) {
+	if (result instanceof Error) {
+	    console.error('Error: ' + response.message);
+	} else {
+	    $ = cheerio.load(result);
+	    var checks = loadChecks(checksfile).sort();
+	    var out = {};
+	    for (var ii in checks) {
+		var present = $(checks[ii]).length > 0;
+		out[checks[ii]] = present;
+	    }
+	    var outJson = JSON.stringify(out, null, 4);
+	    console.log(outJson);
+
+	}    
+	
+    };
+    return response2console;
+};
+
+
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+        .option('-u, --url <url_file>', 'Path to URL') 
+	.parse(process.argv);
+    if (program.url){
+	var response2console = buildfn(program.url, program.checks);
+	rest.get(program.url).on('complete', response2console);
+    } else {
+
+	var checkJson = checkHtmlFile(program.file, program.checks);
+        var outJson = JSON.stringify(checkJson, null, 4);
+	console.log(outJson);
+    }	
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
